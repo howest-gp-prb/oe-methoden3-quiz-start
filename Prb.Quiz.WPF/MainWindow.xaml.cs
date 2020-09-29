6086c3515da2191df76d64a64c1d942795ca4a9b
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-29 08:50:08 +0200

    Namespace adjusted

--- Prb.Quiz.WPF/MainWindow.xaml.cs
+++ Prb.Quiz.WPF/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Methoden3.Quiz.WPF
+namespace Prb.Quiz.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
